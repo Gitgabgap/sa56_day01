318d2b2979b67348ac4ea8234c24a0713e33bc9d
Gabriel <tyrannocorgi123@gmail.com>
2023-06-08 14:58:43 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/sa56_day01_gab/Sa56Day01GabApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.sa56_day01_gab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sa56Day01GabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sa56Day01GabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/sa56_day01_gab/Sa56Day01GabApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.sa56_day01_gab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sa56Day01GabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
